b75fe8525620bf236fafc1833f53c7b5ce76fa5c
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-03 10:47:00 +0200

    Added markup documents to the workspace
    Also made sure these built correctly in the demo workspace.

--- projects/third-cms/src/lib/third-cms.component.ts
+++ projects/third-cms/src/lib/third-cms.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'the-third-cms',
-  template: `
-    <p>
-      the-third-cms works!
-    </p>
-  `,
-  styles: [],
+  templateUrl: './third-cms.component.html',
+  styleUrls: ['./third-cms.component.css'],
 })
 export class ThirdCmsComponent implements OnInit {
   constructor() {}
