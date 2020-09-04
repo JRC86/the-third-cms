8ab14bfea408b8e13e457694b97a7b6f2c8fcabf
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-04 09:51:08 +0200

    Update third-cms.component.ts

--- projects/third-cms/src/lib/third-cms.component.ts
+++ projects/third-cms/src/lib/third-cms.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./third-cms.component.css'],
 })
 export class ThirdCmsComponent implements OnInit {
-  imageUrl: String;
+  imageUrl?: String;
+  textArr?: String[];
+  cmsType: Number; // 0 = Empty, 1 = image, 2 = Text, 3 = ...etc
 
   constructor() {
     this.imageUrl =
       'https://res.cloudinary.com/carlhernek/image/upload/v1594818036/SAR-demo/Restaurants/orlova-maria-oMTlhdFUhdI-unsplash_tfbudv.jpg';
+    this.textArr = ['Paragraph one', 'Paragraph two'];
+    this.cmsType = 0;
   }
 
   ngOnInit(): void {}
