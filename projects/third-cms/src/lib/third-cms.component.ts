683ac0c1550609d154dc37d52f520e8339f75060
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-07 12:23:14 +0200

    Update from Construction Site
    File upload button
    Text editing
    CMS type changing

--- projects/third-cms/src/lib/third-cms.component.ts
+++ projects/third-cms/src/lib/third-cms.component.ts
@@ -7,19 +7,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ThirdCmsComponent implements OnInit {
   // Inputs
-  @Input() iType: any;
-  @Input() iAdmin: any;
+  @Input() Auth: any;
 
   // 1. User is admin and may edit content
   isAdmin: boolean;
+  serviceAdmin: string;
 
   // 2. Toggles for editing UI
   editToggle: boolean;
-  // editToggleArr: String[];
+  textEdit: boolean;
 
   // 3. Content Variables
   imageUrl?: string;
-  textArr?: string[];
+  textString?: string;
+  tempImg: any;
 
   // 4. CMS Type Definition
   cmsType: number; // 0 = Empty, 1 = Image, 2 = Text, 3 = ...etc
@@ -28,6 +29,7 @@ export class ThirdCmsComponent implements OnInit {
   constructor() {
     // 1. User is admin
     this.isAdmin = false;
+    this.serviceAdmin = 'admin@app.com';
 
     // 2. Toggles for editing UI
     this.editToggle = false;
@@ -35,16 +37,15 @@ export class ThirdCmsComponent implements OnInit {
     // 3. Content Variables
     this.imageUrl =
       'https://res.cloudinary.com/carlhernek/image/upload/v1594818036/SAR-demo/Restaurants/orlova-maria-oMTlhdFUhdI-unsplash_tfbudv.jpg';
-    this.textArr = [
-      'Flexitarian air plant readymade williamsburg photo booth biodiesel lomo pork belly knausgaard actually. IPhone hell of iceland, direct trade church-key kombucha polaroid blog fanny pack franzen street art bicycle rights. Butcher tilde 8-bit flexitarian deep v tote bag, cardigan brooklyn messenger bag. Pug viral portland activated charcoal vape. Pork belly scenester glossier quinoa man bun austin salvia umami tilde 3 wolf moon forage kinfolk. Drinking vinegar tumblr slow-carb kale chips fanny pack.',
-      'Yr mustache hoodie austin. Craft beer chicharrones echo park glossier squid fingerstache banjo copper mug neutra slow-carb iceland. Cliche heirloom waistcoat, coloring book man braid la croix actually. 90s chartreuse pour-over freegan cold-pressed kogi la croix shaman pitchfork authentic lomo bicycle rights asymmetrical migas. Street art plaid kombucha, tattooed man bun hoodie humblebrag pabst prism cloud bread hella chia. Celiac selfies sustainable gastropub irony green juice tote bag occupy, iceland put a bird on it prism.',
-    ];
+    this.textString = null;
 
     // 4. CMS Type Definition & Classes
     this.cmsType = 0;
     this.cmsClassDisplay = ['cms-hide', 'cms-hide', 'cms-hide'];
+    this.tempImg = 'upload here';
   }
 
+  // Toggle Editing UI
   editButton = () => {
     if (this.editToggle) {
       this.editToggle = false;
@@ -53,29 +54,60 @@ export class ThirdCmsComponent implements OnInit {
     }
   };
 
+  // Image editing functions
+  onFileChanged(event: any) {
+    const file = event.target.files[0];
+    console.log(file);
+  }
+
+  // Text editing functions
+  editText = () => {
+    if (this.textEdit) {
+      this.formatTextContent();
+      this.textEdit = false;
+    } else {
+      this.textEdit = true;
+      if (!this.textString || this.textString === '') {
+        this.textString = 'Write something';
+      }
+    }
+  };
+
+  setTextContent = (text: string) => {
+    if (text !== this.textString) {
+      this.textString = text;
+    }
+  };
+
+  formatTextContent = () => {
+    let textToFormat = this.textString;
+    const regex = /<br>/gi;
+    this.textString = textToFormat.replace(regex, ' ');
+  };
+
+  // Authorizes the user as admin
   setAdmin = (auth: string) => {
-    if (auth === 'true') {
-      console.log('yay');
+    if (auth === this.serviceAdmin) {
       this.isAdmin = true;
     } else {
       this.isAdmin = false;
     }
   };
 
+  // Changes the CMS type
   setCmsType = (type: number) => {
-    if (type !== 0) {
-      this.cmsClassDisplay[type] = 'cms-show';
+    for (var i = 0; i < this.cmsClassDisplay.length; i++) {
+      this.cmsClassDisplay[i] = 'cms-hide';
+      if (type !== 0 && i === type) {
+        this.cmsClassDisplay[type] = 'cms-show';
+      }
     }
-  };
-
-  parseInput = (input: any) => {
-    let output = parseInt(input);
-    this.cmsType = output;
-    this.setCmsType(this.cmsType);
+    this.cmsType = type;
+    this.editToggle = false;
   };
 
   ngOnInit(): void {
-    this.parseInput(this.iType);
-    this.setAdmin(this.iAdmin);
+    this.setAdmin(this.Auth);
+    this.setCmsType(this.cmsType);
   }
 }
