059e5be4b57af9fb9d46e4f72dae6f5227d2d135
carlhernek <53989735+carlhernek@users.noreply.github.com>
2020-09-07 09:06:01 +0200

    Codebase Update from Construction Site
    Added CMS inputs
    Styling Updates
    UI Conditionals

--- projects/third-cms/src/lib/third-cms.module.ts
+++ projects/third-cms/src/lib/third-cms.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ThirdCmsComponent } from './third-cms.component';
-
-
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ThirdCmsComponent],
-  imports: [
-  ],
-  exports: [ThirdCmsComponent]
+  imports: [CommonModule],
+  exports: [ThirdCmsComponent],
 })
-export class ThirdCmsModule { }
+export class ThirdCmsModule {}
